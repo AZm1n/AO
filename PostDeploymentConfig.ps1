05339f517ea8008b9a689dda0fde232390cfb56c
AZm1n <karim.said@windowslive.com>
2016-05-18 01:43:29 -0700

    dsc

---
+++ PostDeploymentConfig.ps1
@@ -0,0 +1,40 @@
+﻿Configuration PostDeploymentConfig
+{
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            ActionAfterReboot = 'ContinueConfiguration'
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+ 
+        Script FormatDataDisk
+        {
+            SetScript = 
+            { 
+
+
+                $disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+ 
+                 ## start at F: because sometimes E: shows up as a CD drive in Azure 
+                $letters = 70..89 | ForEach-Object { ([char]$_) }
+                $count = 0
+ 
+                foreach($d in $disks) {
+                    $driveLetter = $letters[$count].ToString()
+                    $d | 
+                    Initialize-Disk -PartitionStyle MBR -PassThru |
+                    New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+                    Format-Volume -FileSystem NTFS `
+                        -Confirm:$false -Force 
+                    $count++
+                                    }
+
+                
+            }
+            TestScript = { $false }
+            GetScript = { @{ Result = "" } }
+        }
+    }
+}
