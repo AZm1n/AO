d70de51998ee1be858abc15801a20379eed1db62
AZm1n <karim.said@windowslive.com>
2016-05-18 23:20:03 -0700

    DSC

--- PostDeploymentConfig.ps1
+++ PostDeploymentConfig.ps1
@@ -1,79 +1,20 @@
-Configuration PostDeploymentConfig
-{
-
-    param
+﻿    param
     (
         [Parameter(Mandatory)]
-        [String[]]$Disks,
-        [Int]$RetryCount=3,
-        [Int]$RetryIntervalSec=30,
-<<<<<<< HEAD
-        [Int]$i=2
+        [String[]]$Letters
     )
 
-=======
-        [Int]$Counts=2
-    )
 
-    Import-DscResource -ModuleName xComputerManagement,CDisk,XDisk,xNetworkin
-   
-       
->>>>>>> dfc220d84a6ed24917e4ec643d30936b23282c02
-    Node localhost
-    {
-    
-        LocalConfigurationManager
-        {
-            ActionAfterReboot = 'ContinueConfiguration'
-            ConfigurationMode = 'ApplyOnly'
-            RebootNodeIfNeeded = $true
-        }
- 
-        Script FormatDataDisk
-        {
-            SetScript = 
-            { 
-<<<<<<< HEAD
-                $Disks.ForEach({
-                
-                $count = 2
-                
-                    $driveLetter = $_
-                    
-                    Initialize-Disk -Number $count  -PartitionStyle MBR -PassThru |
-                    New-Partition -UseMaximumSize -DriveLetter $driveLetter |
-                    Format-Volume -FileSystem NTFS -Confirm:$false -Force 
+$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+## start at F: because sometimes E: shows up as a CD drive in Azure 
+$letters = 70..89 | ForEach-Object { ([char]$_) }
+$count = 0
+foreach($d in $disks) {
+$driveLetter = $Letters[$count].ToString()
+$d | 
+Initialize-Disk -PartitionStyle MBR -PassThru |
+New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+                    Format-Volume -FileSystem NTFS `
+                        -Confirm:$false -Force 
                     $count++
-                               })
-=======
-
-                $Disks.ForEach({
-
-                xWaitforDisk Disk
-
-                {
-
-                DiskNumber = $Counts
-                RetryIntervalSec = $RetryIntervalSec
-                Count = $RetryCount
-
-                }
-
-                cDiskNoRestart Disk
-
-                {
-
-                DiskNumber = $Counts
-                DriveLetter = $_
-                
-                }
-                $Counts++
-            })
-
->>>>>>> dfc220d84a6ed24917e4ec643d30936b23282c02
-            }
-            TestScript = { $false }
-            GetScript = { @{ Result = "" } }
-        }
-    }
-}
+                                    }
