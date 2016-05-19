4777271176cc05b2d731873963d6d6e72ff4dff7
AZm1n <karim.said@windowslive.com>
2016-05-18 22:04:46 -0700

    dsc

--- PostDeploymentConfig.ps1
+++ PostDeploymentConfig.ps1
@@ -1,7 +1,18 @@
 ﻿Configuration PostDeploymentConfig
 {
+
+    param
+    (
+        [Parameter(Mandatory)]
+        [String[]]$Disks,
+        [Int]$RetryCount=3,
+        [Int]$RetryIntervalSec=30,
+        [Int]$i=2
+    )
+
     Node localhost
     {
+    
         LocalConfigurationManager
         {
             ActionAfterReboot = 'ContinueConfiguration'
@@ -13,25 +24,17 @@
         {
             SetScript = 
             { 
-
-
-                $disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
- 
-                 ## start at F: because sometimes E: shows up as a CD drive in Azure 
-                $letters = 70..89 | ForEach-Object { ([char]$_) }
-                $count = 0
- 
-                foreach($d in $disks) {
-                    $driveLetter = $letters[$count].ToString()
-                    $d | 
-                    Initialize-Disk -PartitionStyle MBR -PassThru |
-                    New-Partition -UseMaximumSize -DriveLetter $driveLetter |
-                    Format-Volume -FileSystem NTFS `
-                        -Confirm:$false -Force 
-                    $count++
-                                    }
-
+                $Disks.ForEach({
                 
+                $count = 2
+                
+                    $driveLetter = $_
+                    
+                    Initialize-Disk -Number $count  -PartitionStyle MBR -PassThru |
+                    New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+                    Format-Volume -FileSystem NTFS -Confirm:$false -Force 
+                    $count++
+                               })
             }
             TestScript = { $false }
             GetScript = { @{ Result = "" } }
