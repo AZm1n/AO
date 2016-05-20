0d1758c9835304294a8ef9aa6e6c800dec46b01e
AZm1n <karim.said@windowslive.com>
2016-05-19 20:11:42 -0700

    DSC

--- PostDeploymentConfig.ps1
+++ PostDeploymentConfig.ps1
@@ -4,17 +4,19 @@
         [String[]]$Letters
     )
 
+h
+
 
 $disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
-## start at F: because sometimes E: shows up as a CD drive in Azure
+## start at F: because sometimes E: shows up as a CD drive in Azure 
+$Letters | ForEach-Object { ([char]$_) }
+$count = 0
 
-$Letters.Split("{,}")
-
-$count = 2
-foreach($l in $Letters) {
-
-Initialize-Disk -Number $count -PartitionStyle MBR -PassThru |
-New-Partition -UseMaximumSize -DriveLetter $l |
+foreach($d in $disks) {
+$driveLetter = $Letters[$count].ToString()
+$d | 
+Initialize-Disk -PartitionStyle MBR -PassThru |
+New-Partition -UseMaximumSize -DriveLetter $driveLetter |
                     Format-Volume -FileSystem NTFS `
                         -Confirm:$false -Force 
                     $count++
