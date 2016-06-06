d85488eed865d770889db72a138d3b020856686e
AZm1n <karime@microsoft.com>
2016-06-06 14:33:37 -0700

    dsc

---
+++ SqlDeployment.ps1
@@ -0,0 +1,22 @@
+﻿    param
+    (
+        [String]$Letters,
+        [String]$AdminGroup
+    )
+
+[string[]]$Letters = $Letters.Replace("'","").Split(",") #converting the single string into an array of strings
+
+$count = 0
+
+
+foreach ($disk in get-wmiobject Win32_DiskDrive -Filter "Partitions = 0"){
+$driveLetter = $Letters[$count].ToString()
+
+   $disk.DeviceID
+   $disk.Index
+   "select disk "+$disk.Index+"`r clean`r create partition primary`r format fs=ntfs unit=65536 quick`r active`r assign letter=$driveLetter" | diskpart
+	$count++
+}
+
+#Add group to local administrators group.
+net localgroup Administrators /add $AdminGroup
