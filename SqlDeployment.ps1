d99f16aebdcf65785b1b9e271fa0ee536400b604
AZm1n <karim.said@windowslive.com>
2016-06-14 18:57:21 -0700

    dsc

--- SqlDeployment.ps1
+++ SqlDeployment.ps1
@@ -10,12 +10,14 @@
 
 
 #Unmount CD/DVD drive d:
+Write-Host "Unmounting CD/DVD drive"
 mountvol E: /D
 
 [string[]]$Letters = $Letters.Replace("'","").Split(",") #converting the single string into an array of strings
 
 $count = 0
 
+Write-Host "Starting Disk format...please wait"
 
 foreach ($disk in get-wmiobject Win32_DiskDrive -Filter "Partitions = 0"){
 $driveLetter = $Letters[$count].ToString()
@@ -27,16 +29,20 @@ $driveLetter = $Letters[$count].ToString()
 }
 
 #Set the time zone
+Write-Host "Setting up the timezone"
 TZUTIL /s "Pacific Standard Time"
 
 #Add group to local administrators group.
 [string[]]$Admins = $AdminGroup.Replace("'","").Split(";")
 
+Write-Host "Addint the users to Adminstrators group"
 foreach ($Admin in $Admins)
 {
 net localgroup Administrators /add $Admin
 }
 #Create SQL Directories
+
+Write-Host "Creating SQL directories"
 
 md H:\MSSQL\DATA
 md O:\MSSQL\DATA
@@ -44,17 +50,28 @@ md E:\MSSQL\BAK
 
 
 #Install SQL Framework Core
+Write-Host "Installing framework 3.5"
 Install-WindowsFeature Net-Framework-Core -source $SxsSource
 
+#Pause for 30 seconds after the framework install
+Write-Host "Starting sleeping for 60 seconds"
+Start-Sleep -Seconds 60
 
 #Map Net Drive
-net use z: $IpakServer
+#net use z: $IpakServer
+Write-Host "Mapping networ drive"
+New-PSDrive -Name "Z" -PSProvider FileSystem -Root $IpakServer
+
 
 #Construct the commandline argument
-$sqlcmd = "z:\SQL2014SP1\sqlipak.exe"
+Write-Host "Running the IPAK..."
+$sqlcmd = "Z:\SQL2014SP1\sqlipak.exe"
 &$sqlcmd /SQL /BIN:C: /DAT:H: /TRAN:O: /BAK:E: /TEMP:D: /QFE:12.0.4422 /RemoveBuiltin "$SqlAdmins" /AUTOTEMPFILES /CLEANMSDB /NOLOGCOPY
 
+
 #Add SQL startup Script
+Write-Host "Adding SQL startup scripts"
+
 md C:\SQLStartup
 Copy-Item -Path $SxsSource\SQL-Startup.ps1 -Destination C:\SQLStartup
 &C:\SQLStartup\SQL-Startup.ps1
